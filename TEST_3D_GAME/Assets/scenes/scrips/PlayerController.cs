bef118f178e21c7fff49ef16ee12d90adf5f0541
GVatinno <gianluca.vatinno@gmail.com>
2015-06-16 08:32:17 +0100

    first commit
    first commit

---
+++ TEST_3D_GAME/Assets/scenes/scrips/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		float x = Input.GetAxis("Horizontal") * 2.5f;
+		float y = Input.GetAxis("Vertical") * 2.5f;
+
+		rb.AddForce ( x, 0.0f, y);
+
+	}
+}
